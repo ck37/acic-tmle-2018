2409a7b7199f8bc1db187cb935c56cf5d6475383
Chris Kennedy <chrisken@gmail.com>
2018-05-06 13:57:54 -0700

    Create structure for the entry pipeline, to be filled in

--- R/run-analysis.R
+++ R/run-analysis.R
@@ -2,8 +2,17 @@
 run_analysis = function(
   input_dir_counterfactuals = "data-raw/practice-censoring",
   input_file_covariates = "data-raw/x.csv",
-  output_file_ate = "exports/ate-test.csv",
-  output_dir_ipo = "exports/ipo-test",
   verbose = TRUE) {
+  
+  # Inputs are all *.csvs that don't end in _cf.csv
+  # The _cf.csv files are for checking our predictions
+  # File with covariate values
+  # Looping over all counterfactual files
+  # Data setup
+  # Import covariate dataset
+  # Import one counterfactual file
+  # Merge counterfactuals with the covariate dataset based on sample_ID
+  # Run TMLE analysis
+  # Return results
   
 }
