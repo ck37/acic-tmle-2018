0b7c65ddbdbcae86c55872d06306c044c9da4831
Chris Kennedy <chrisken@gmail.com>
2018-05-06 15:10:27 -0700

    process-covariates: tweak interface

--- R/process-covariates.R
+++ R/process-covariates.R
@@ -2,14 +2,17 @@
 #' TODO: upgrade to more thoroughly handle the different covariates.
 process_covariates = function(data, id_field = "sample_id", verbose = FALSE) {
   # Extract the sample ids.
-  id = data[[id_file]] 
+  # TODO: check if id_field exists in this data.
+  id = data[[id_field]] 
   
   # Remove ID field from our dataset.
-  covariates = data[, setdiff(colnames(data), id_field)]
+  # Actually we need to keep it in for later merging.
+  # covariates = data[, setdiff(colnames(data), id_field), drop = FALSE]
   
   # Compile results.
   results = list(
-    covariates = covariates,
+    data = data,
+    covariates = setdiff(colnames(data), id_field),
     id = id
   )
   
