68f04d283c9ea83716418b1c6bb088d408cb2ce9
Chris Kennedy <chrisken@gmail.com>
2018-05-06 13:49:38 -0700

    rename directories

---
+++ R/run-analysis.R
@@ -0,0 +1,9 @@
+#' TODO: document function
+run_analysis = function(
+  input_dir_counterfactuals = "data-raw/practice-censoring",
+  input_file_covariates = "data-raw/x.csv",
+  output_file_ate = "exports/ate-test.csv",
+  output_dir_ipo = "exports/ipo-test",
+  verbose = TRUE) {
+  
+}
