1425711c2fe2b7f6198a7483c02daa489fd6b1c8
Chris Kennedy <chrisken@gmail.com>
2018-05-06 14:04:50 -0700

    newer version of load_all_packages() and startup()

--- R/function_library.R
+++ R/function_library.R
@@ -1,84 +1,124 @@
-#' Load all packages needed, installing any missing packages from CRAN.
+#' Load all packages needed, installing any missing packages if desired.
 #'
 #' @param auto_install Install any packages that could not be loaded.
 #' @param update Update any packages that can be updated.
 #' @param java_mem Amount of RAM to allocate to rJava; must happen before
-#'   library is loaded.
-load_all_packages = function(auto_install = F, update = F, java_mem = "4g", verbose = F) {
+#'   library is loaded. If it happens afterwards it will not change available memory.
+#' @param verbose If TRUE display extra output during execution.
+load_all_packages =
+  function(packages_cran = NULL,
+           packages_github = NULL,
+           auto_install = FALSE,
+           update = FALSE,
+           java_mem = "2g",
+           verbose = FALSE) {
+    
   if (verbose) {
     # Output R version so we know which package versions we're using.
     cat(R.version.string, "\n")
   }
-
+  
+  if (verbose) {
+    cat("CRAN packages:", paste(packages_cran, collapse = ", "), "\n")
+    cat("Github packages:", paste(packages_github, collapse = ", "), "\n")
+  }
+  
   # Allocate 4GB to Java for bartMachine; needs to happen before we load rJava library.
-  options(java.parameters = paste0("-Xmx", java_mem))
-
-  libs = c(
-    # Warning: This will crash R if a JDK is not installed.
-    "bartMachine",
-    "caret",
-    "doMC",
-    #"doParallel",
-    #"doSNOW",
-    #"e1071",
-    "earth",
-    #"gam",
-    #"gbm",
-    #"gee",
-    "ggplot2",
-    "glmnet",
-    #"KernelKnn",
-    "kernlab",
-    "mgcv",
-    "nnet",
-    #"pROC",
-    "randomForest",
-    "ranger",
-    "RhpcBLASctl",
-    #"ROCR",
-    #"rpart",
-    #"sandwich",
-    #"speedglm",
-    "xgboost"#,
-    #"xtable"
-  )
-
+  # TODO: may want to defer this step (and loading rJava) until we have setup parallelization.
+  if (!is.null(java_mem)) {
+    options(java.parameters = paste0("-Xmx", java_mem))
+  }
+  
   # Code is not yet run. We run afterward, possibly with messages suppressed.
   expression = quote({
-
+    
     # Install devtools if we don't already have it.
     if (!require("devtools") && auto_install) {
       if (verbose) cat("No devtools detected - installing.\n")
       install.packages("devtools")
       library(devtools)
     }
-
-
-    # NOTE: may want to install the latest xgboost from github.
-    # Can run this manually:
-    if (!require("xgboost") && auto_install) {
-      if (verbose) cat("No xgboost detected - installing.\n")
-      install.packages("xgboost",
-                       repos=c("http://dmlc.ml/drat/", getOption("repos")),
-                       type="source")
-    }
-
+    
     # Install any packages from github that are needed.
-    if (auto_install) {
-      devtools::install_github(c("ecpolley/SuperLearner",
-                                 "ck37/ck37r"))
+    if (length(packages_github) > 0) {
+      if (auto_install) {
+        devtools::install_github(packages_github)
+      }
+      
+      if (is.null(names(packages_github))) {
+        # Extract package name as the repo name of each package, if the vector itself is not named.
+        # This assumes github packages are of the form: username/repo@whatev (@whatev optionally included).
+        github_names = gsub("^[^/]+/([^@]+)", "\\1", packages_github)
+      } else {
+        # Support a named vector, in case the package name is not the same as the repo name.
+        github_names = names(packages_github)
+      }
+      invisible(sapply(github_names, require, character.only = T))
     }
-    invisible(sapply(c("SuperLearner", "ck37r"), require, character.only = T))
-
-    ck37r::load_packages(libs, auto_install, update, verbose = verbose)
+    
+    # This part clearly requires that the ck37r package was already installed,
+    # either via packages_cran or packages_github.
+    ck37r::load_packages(packages_cran, auto_install, update, verbose = verbose)
   })
-
+  
   # Now run the stored code either directly or with messages suppressed.
   if (verbose) {
     # Allow messages to be output.
     eval(expression)
   } else {
-    # Supress messages.
+    # Suppress messages.
     suppressMessages(eval(expression))
   }
 }
+
+
+#' @param packages_cran Character vector of CRAN packages to load, and possibly install if needed.
+#' @param packages_github Character vector of github packages to load, and possibly install if needed.
+startup = function(packages_cran = NULL,
+                   packages_github = NULL,
+                   ...) {
+  if (is.null(packages_cran)) {
+    # Extract defaults based on an attribute so that it can be examined/modified if needed.
+    packages_cran = attr(startup, "packages_cran")
+  }
+  
+  if (is.null(packages_github)) {
+    # Extract defaults based on an attribute so that it can be examined/modified if needed.
+    packages_github = attr(startup, "packages_github")
+  }
+  
+  load_all_packages(packages_cran = packages_cran,
+                    packages_github = packages_github, ...)
+}
+
+# Specify defaults as attributes so that they can be extracted programmatically.
+attr(startup, "packages_cran") = c(
+  "here",
+  "data.table",
+  "magrittr", # For %$%
+  "rlang",
+  "knitr",
+  "rmarkdown",
+  "doParallel",
+  "stargazer",
+  "RhpcBLASctl",
+  
+  # Visualization
+  "ggplot2",
+  
+  # Machine learning.
+  "bartMachine",
+  "caret",
+  "glmnet",
+  #"mgcv",
+  "xgboost",
+  "ranger"
+)
+
+attr(startup, "packages_github") = c(
+  "ecpolley/SuperLearner",
+  "tlverse/sl3",
+  "ck37/ck37r"#,
+  #"osofr/stremr"
+  # TODO: add gentmle2
+)
