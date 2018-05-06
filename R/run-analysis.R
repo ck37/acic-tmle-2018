9abb12fdfbfc4efa8b21c0d0e99d308cfd09ff1b
Chris Kennedy <chrisken@gmail.com>
2018-05-06 15:09:52 -0700

    barebones run_analysis() is setup; need to add TMLE function

--- R/run-analysis.R
+++ R/run-analysis.R
@@ -7,6 +7,7 @@ run_analysis =
     input_pattern = "(?<!_cf)\\.csv$",
     input_cf_pattern = "_cf.csv$",
     input_file_covariates = "data-raw/x.csv",
+    id_field = "sample_id",
     verbose = TRUE) {
   
   # Check that input_dir_counterfactuals exists
@@ -27,7 +28,7 @@ run_analysis =
   # The _cf.csv files are for checking our predictions
   cf_files = all_files[grepl(input_cf_pattern, all_files, perl = TRUE)]
   if (verbose) {
-    cat("Found", length(cf_files), "true counterfactual files for evaluation.")
+    cat("Found", length(cf_files), "true counterfactual files for evaluation.\n")
   }
   
   # Check that input_file_covariates exists
@@ -36,23 +37,70 @@ run_analysis =
   }
   
   # Import covariate dataset
-  covariates = rio::import(input_file_covariates)
+  covariate_df = rio::import(input_file_covariates)
   
   # Process covariate file. Should return a list with at least $covariates and $id.
-  results = process_covariates(covariates, verbose = verbose)
-  covariates = results$covariates
+  results = process_covariates(covariate_df, verbose = verbose)
+  covariate_df = results$data
   id = results$id
   
-  # Looping over all counterfactual files
+  
+  # Dataframe to save ATE estimates for each file.
+  ate_df = NULL
+  # List of individual potential outcome estimates for each file.
+  ipo_list = list()
+  
+  # Looping over all counterfactual files.
   for (file in files) {
+    # ufid = filename without the enclosing directory.
+    ufid = gsub("^.*/([^./]+?)\\.csv$", "\\1", file, perl = TRUE)
+    
+    if (verbose) {
+      cat("Processing", ufid, "\n")
+    }
+    
     # Import one counterfactual file
-    # Merge counterfactuals with the covariate dataset based on sample_ID
-    # Run TMLE analysis
+    # Column names are sample_id, z (treatment), and y (outcome)
+    cf = rio::import(file)
+    
+    # Merge into single dataset
+    data = merge(cf, covariate_df, by.x = id_field, by.y = id_field,
+                 # Keep all rows in CF, but ok if we drop rows in covariates.
+                 all.x = TRUE, all.y = FALSE)
+    
+    # Run TMLE analysis.
+    # TODO: determine function to run; pass in appropriate data.
+    # Should return population ATE with inference, plus df of individual potential outcomes.
+    
+    # Temporary placeholder.
+    tmle_result = list(ate_est = NULL, ci_left = NULL, ci_right = NULL,
+                       ipo_df = data.frame())
+    
+    ate_result =
+      list(ufid = ufid,
+           # Population ATE.
+           effect_size = tmle_result$ate_est,
+           # Left confidence interval.
+           li = tmle_result$ci_left,
+           # Right confidence interval.
+           ri = tmle_result$ci_right)
+    
+    # Integrate into dataframe for the ATEs.
+    ate_df = rbind(ate_df, ate_result)
+    
+    # Save individual potential outcome result.
+    ipo_list[[ufid]] = tmle_result$ipo_df
   }
   
-  # Return results
-  # - ATE, CI
-  # - Individual potential outcomes dataframe
-  # Counterfactul dataframe
+  # Compile results.
+  results = list(
+    # ATE df includes the point estimate and confidence interval
+    ate = ate_df,
+    # Each list element is a dataframe of potential outcomes for a given file.
+    ipos = ipo_list,
+    # Set of counterfactual files that can be used for evaluation.
+    cf_files = cf_files
+  )
   
+  return(results)
 }
