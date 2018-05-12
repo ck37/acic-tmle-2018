321f01ac8e3279ba48ada19dd73bf0d567a238eb
Chris Kennedy <chrisken@gmail.com>
2018-05-12 14:23:07 -0700

    wrapper-tmle-glm: fix for revised covariate screening format

--- R/wrapper-tmle-glm.R
+++ R/wrapper-tmle-glm.R
@@ -15,10 +15,30 @@ wrapper_tmle_glm =
   ##############
   # Define all the required elements to pass into tmle()
   
+  # TODO: work on this section so that fewer variable names are created.
+  # It's somewhat duplicative/roundabout at the moment.
   if (is.null(covariate_fields)) {
-    covariate_fields = setdiff(colnames(data),
-                               c(outcome_field, treatment_field, id_field))
+    covariate_fieldsY = setdiff(colnames(data),
+                                c(outcome_field, treatment_field, id_field))
+    covariate_fieldsA = setdiff(colnames(data),
+                                c(outcome_field, treatment_field, id_field))
+    covariate_fieldsC = setdiff(colnames(data),
+                                c(outcome_field, treatment_field, id_field))
+  }else{
+    covariate_fieldsY = covariate_fields$outcome
+    covariate_fieldsA = covariate_fields$treatment
+    covariate_fieldsC = covariate_fields$censoring
   }
+  
+  # Include treatment indicator in outcome regression.
+  covariates_Q = c(treatment_field, covariate_fieldsY)
+  covariates_g = covariate_fieldsA
+  covariates_c = covariate_fieldsC
+  
+  # TODO: need to create screeners for each of these covariate sets
+  # that are applied to the SuperLearner libraries for the respective
+  # fits(Q, g, c).
+
     
   #####
   # Setup SL libraries.
@@ -39,9 +59,12 @@ wrapper_tmle_glm =
   
   #####
   # Run estimator
+  
+  # TODO: conduct SL estimation outside of tmle() so that we can use our
+  # covariate subsets, conduct stratified CV, etc.
   tmle_result = tmle(Y = data[[outcome_field]],
                      A = data[[treatment_field]],
-                     W = data[, covariate_fields, drop = FALSE],
+                     W = data[, unique(c(covariates_Q, covariates_g, covariates_c)), drop = FALSE],
                      Delta = as.integer(!is.na(data[[outcome_field]])),
                      Q.SL.library = q_lib,
                      g.SL.library = g_lib,
