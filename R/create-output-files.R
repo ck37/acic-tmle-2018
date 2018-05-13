677b421ca4feb5c40419c0ebf19daf973666ab0c
Caleb H. Miles <calebhmiles@gmail.com>
2018-05-13 15:14:39 -0700

    Make file names in zip folder consistent with contest expectations, i.e., remove filepath within zip folder files

--- R/create-output-files.R
+++ R/create-output-files.R
@@ -11,13 +11,16 @@ create_output_files =
   write.csv(results$ate, file = output_file_ate)
     
   # (2) individual potential outcomes for each individual data file
-  dir.create(output_dir_ipo)
-  dir.create(file.path(output_dir_ipo, "submission"), showWarnings = FALSE)
+  dir.create(output_dir_ipo, showWarnings = FALSE)
   newfiles = NULL
   for (ufid in names(results$ipos)) {
-    newfiles = c(newfiles, paste0(output_dir_ipo, "/submission/", ufid, ".csv"))
-    write.csv(results$ipos[[ufid]], file = paste0(output_dir_ipo, "/submission/", ufid, ".csv"))
+    newfiles = c(newfiles, paste0(ufid, ".csv"))
+    write.csv(results$ipos[[ufid]], file = paste0(output_dir_ipo, "/", ufid, ".csv"))
   }
   # Put these in a .zip file
-  zip(paste0(output_dir_ipo, "/submission.zip"), newfiles)
+  temp_wd = getwd()
+  setwd(output_dir_ipo)
+  zip("submission.zip", newfiles)
+  setwd(temp_wd)
+  rm(temp_wd)
 }
