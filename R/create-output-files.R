79a236751a5281e0e800ef8a0523f60bc78b9fec
Caleb H. Miles <calebhmiles@gmail.com>
2018-05-13 14:44:27 -0700

    Eliminate use of $cf_files in producing ipo files

--- R/create-output-files.R
+++ R/create-output-files.R
@@ -8,18 +8,16 @@ create_output_files =
     
   # Write separate .csv files for:
   # (1) average treatment effects and confidence intervals for all data files
-  write.csv(results$ate_df, file = output_file_ate)
+  write.csv(results$ate, file = output_file_ate)
     
-  # (2) individual potential outcomes for each individual data file, 
-  # TODO: this should use the output_dir_ipo function argument.
-  dir.create(file.path("exports", "submission"), showWarnings = FALSE)
+  # (2) individual potential outcomes for each individual data file
+  dir.create(output_dir_ipo)
+  dir.create(file.path(output_dir_ipo, "submission"), showWarnings = FALSE)
   newfiles = NULL
-  # TODO: should not be using $cf_files, doesn't exist for the real data.
-  for (file in results$cf_files) {
-    ufid = gsub("^.*/([^./]+?)\\_cf.csv$", "\\1", file, perl = TRUE)
-    newfiles = c(newfiles, paste0(output_dir_ipo, "/", ufid, ".csv"))
-    write.csv(results$ipos[[ufid]], file = paste0(output_dir_ipo, "/", ufid, ".csv"))
+  for (ufid in names(results$ipos)) {
+    newfiles = c(newfiles, paste0(output_dir_ipo, "/submission/", ufid, ".csv"))
+    write.csv(results$ipos[[ufid]], file = paste0(output_dir_ipo, "/submission/", ufid, ".csv"))
   }
   # Put these in a .zip file
-  zip("exports/submission.zip", newfiles)
+  zip(paste0(output_dir_ipo, "/submission.zip"), newfiles)
 }
