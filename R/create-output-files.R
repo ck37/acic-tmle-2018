5a6c0f2d2ec3ad3864e6f334fc01065f7ac5de30
Caleb H. Miles <calebhmiles@gmail.com>
2018-05-12 20:59:28 -0700

    Set up file exporting

--- R/create-output-files.R
+++ R/create-output-files.R
@@ -4,4 +4,17 @@ create_output_files =
     output_file_ate,
     output_dir_ipo,
     verbose = FALSE) {
+  # Write separate .csv files for:
+  # (1) average treatment effects and confidence intervals for all data files, and
+    write.csv(results$revere$ate_df, file = output_file_ate)
+    
+  # (2) individual potential outcomes for each individual data file, 
+    newfiles = NULL
+    for(file in results$cf_files){
+      ufid = gsub("^.*/([^./]+?)\\_cf.csv$", "\\1", file, perl = TRUE)
+      newfiles = c(newfiles, paste0("exports/submission/", ufid, ".csv"))
+      write.csv(results$ipos[[ufid]], file = paste0("exports/submission/", ufid, ".csv"))
+    }
+  # Put these in a .zip file
+    zip("exports/submission.zip", newfiles)
 }
